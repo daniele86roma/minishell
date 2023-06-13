a693171289b1f1053d08578814b30d72a61b31d0
adi-fort <113356093+adi-fort@users.noreply.github.com>
2023-06-13 11:14:56 +0200

    Add files via upload

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: dfiliagg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 09:17:05 by dfiliagg          #+#    #+#             */
-/*   Updated: 2023/06/07 18:23:43 by adi-fort         ###   ########.fr       */
+/*   Updated: 2023/06/13 10:40:21 by adi-fort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,45 @@ void	init(char *envp[], t_pipex *pipex, int argc, char **argv)
 {
 	(void)argv;
 	if (argc != 1)
-		exit(write(2, "Error: Argument\n", 16));
+	{
+		write(2, "Error: Argument\n", 16);
+		exit (2);
+	}
 	save_io(pipex);
 	ft_create_envp(pipex, envp);
 	path(pipex);
 	pipex->commands = 0;
 	pipex->args = 0;
+	ft_signal();
+}
+
+void	compact_main(t_pipex *pipex)
+{
+	char	*s;
+
+	add_history(pipex->input);
+	pipex->mat = create_matrix(pipex->input);
+	var_mat(pipex, pipex->mat);
+	s = mat_to_string(pipex->mat);
+	free_mat(pipex->mat);
+	pipex->mat = create_matrix(s);
+	free(s);
+}
+
+void	compact_main2(t_pipex *pipex)
+{
+	parse(pipex->mat, pipex);
+	exe(pipex);
+	free_mat(pipex->mat);
+	free(pipex->input);
+	free_path(pipex);
+	path(pipex);
+}
+
+void	compact_main3(t_pipex *pipex)
+{
+	pipex->exit_builtin = 0;
+	pipex->input = readline("Minishell> ");
 }
 
 int	main(int argc, char **argv, char *envp[])
@@ -31,35 +64,25 @@ int	main(int argc, char **argv, char *envp[])
 	t_pipex	pipex;
 
 	init(envp, &pipex, argc, argv);
-	ft_signal();
 	while (1)
 	{
-		create_exit_status(&pipex);
-		pipex.input = readline("MiniShell> ");
+		compact_main3(&pipex);
 		if (!pipex.input)
 		{
+			write(1, "\n", 1);
 			free_total(&pipex);
 			exit(0);
 		}
 		else if (pipex.input[0] == 0)
 			continue ;
-		add_history(pipex.input);
-		pipex.mat = create_matrix(pipex.input);
-		var_mat(&pipex, pipex.mat);
-		if (pipex.mat[0] && (check_token(&pipex) || check_quote(pipex.mat)))
+		compact_main(&pipex);
+		if (pipex.mat[0] && check_quote(pipex.mat))
 		{
 			free(pipex.input);
 			free_mat(pipex.mat);
 			continue ;
 		}
-		parse(pipex.mat, &pipex);
-		exe(&pipex);
-		add_exitstatus(&pipex);
-		g_exitcode = 0;
-		free_mat(pipex.mat);
-		free(pipex.input);
-		free_path(&pipex);
-		path(&pipex);
+		compact_main2(&pipex);
 	}
 	free_total(&pipex);
 	return (0);
